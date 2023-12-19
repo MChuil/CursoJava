46eade769db44e27c8ac05fc4cab88ce57fdec99
Miguel Chuil <developer.chuil@gmail.com>
2023-12-19 12:19:41 -0600

    Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,31 @@
+public class HolaMundo{
+
+    int edad = 10;
+    double pi = 3.1416;
+    boolean casado = false;
+    String saludo = "Hola Mundo...";
+    //constante
+    final String name = "Miguel Martinez";
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java...!");
+    }
+
+    /*
+     * ------------ TIPO DE DATOS ---------------
+     *  1) Tipos primitivos
+     *      - enteros : byte, short, char, int, long
+     *      - flotantes : float, double
+     *      - booleanos : true, false
+     * 
+     *  2) Tipos referenciados
+     *      - clases
+     *      - interfaces
+     *      - arreglos
+     */
+
+     /*
+      *     String
+      *     String saludo = "Hola Mundo...";
+      *     saludo.equals("Hola Mundo");
+      */
+}
